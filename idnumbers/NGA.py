ae4499f4465bdcb5064648194a5d0579a08ba886
microdataxyz <microdataxyz@outlook.com>
2022-12-15 08:04:48 +0800

    restyle the code

--- idnumbers/NGA.py
+++ idnumbers/NGA.py
@@ -1,23 +1,25 @@
 import re
 from types import SimpleNamespace
 
-"""
-Nigeria ID number format
-https://en.wikipedia.org/wiki/National_identification_number#Nigeria
-"""
-METADATA = SimpleNamespace(**{
-    'iso3166_alpha2': 'NG',
-    'min_length': 11,
-    'max_length': 11
-})
 
-REGEXP = re.compile(r'^\d{11}$')
-
-
-def validate(id_number: str) -> bool:
+class NationalID:
     """
-    Validate the NGA id number
+    Nigeria National ID number format
+    https://en.wikipedia.org/wiki/National_identification_number#Nigeria
     """
-    if not isinstance(id_number, str):
-        id_number = repr(id_number)
-    return REGEXP.search(id_number) is not None
+    METADATA = SimpleNamespace(**{
+        'iso3166_alpha2': 'NG',
+        'min_length': 11,
+        'max_length': 11
+    })
+
+    REGEXP = re.compile(r'^\d{11}$')
+
+    @staticmethod
+    def validate(id_number: str) -> bool:
+        """
+        Validate the NGA id number
+        """
+        if not isinstance(id_number, str):
+            id_number = repr(id_number)
+        return NationalID.REGEXP.search(id_number) is not None
--- idnumbers/test_NGA.py
+++ idnumbers/test_NGA.py
@@ -4,14 +4,20 @@ from . import NGA
 
 class TestNGAValidation(TestCase):
     def test_normal_case(self):
-        self.assertTrue(NGA.validate('12345678901'))
+        self.assertTrue(NGA.NationalID.validate('12345678901'))
 
     def test_error_case(self):
-        self.assertFalse(NGA.validate('1234567890A'))
+        self.assertFalse(NGA.NationalID.validate('1234567890A'))
 
     def test_number_type(self):
         # if the user doesn't follow the type hinting, we still handle it
-        self.assertTrue(NGA.validate(12345678901))
+        self.assertTrue(NGA.NationalID.validate(12345678901))
+
+    def test_with_regex(self):
+        self.assertRegex('12345678901', NGA.NationalID.REGEXP)
+
+    def test_with_metadata(self):
+        self.assertIsNotNone(NGA.NationalID.METADATA)
 
 
 if __name__ == '__main__':
