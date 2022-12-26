39e91773d55c54410d51f8812c81ec9eef0dc026
microdataxyz <120368198+microdataxyz@users.noreply.github.com>
2022-12-26 11:32:13 +0800

    Fix #30 - implement Taiwan national id (#90)

---
+++ idnumbers/nationalid/test_TWN.py
@@ -0,0 +1,26 @@
+from unittest import TestCase, main
+
+from idnumbers.nationalid import TWN
+from idnumbers.nationalid.constant import Gender
+
+
+class TestTHAValidation(TestCase):
+    def test_normal_case(self):
+        self.assertTrue(TWN.NationalID.validate('A123456789'))
+        self.assertTrue(TWN.NationalID.validate('M140051653'))
+        self.assertTrue(TWN.NationalID.validate('Q238927307'))
+
+    def test_error_case(self):
+        self.assertFalse(TWN.NationalID.validate('A223456789'))
+        self.assertFalse(TWN.NationalID.validate('m140051653'))
+
+    def test_parse(self):
+        result = TWN.NationalID.parse('A123456789')
+        self.assertEqual('A', result['location'])
+        self.assertEqual(Gender.MALE, result['gender'])
+        self.assertEqual('2345678', result['sn'])
+        self.assertEqual('9', result['checksum'])
+
+
+if __name__ == '__main__':
+    main()
