095ec14622224556b01da636fa5bf10d2b4417ff
Angus Hsu <apangus611@gmail.com>
2022-12-27 15:37:36 +0800

    Fix #18 implement JPN national ID

---
+++ idnumbers/nationalid/test_JPN.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, main
+from idnumbers.nationalid import JPN
+
+
+class TestJPNValidation(TestCase):
+    def test_normal_case(self):
+        self.assertTrue(JPN.NationalID.validate('765895492872'))
+
+    def test_error_case(self):
+        self.assertFalse(JPN.NationalID.validate('123456789012'))
+        self.assertFalse(JPN.NationalID.validate('1234567890123'))
+
+    def test_with_regex(self):
+        self.assertRegex('765895492872', JPN.NationalID.METADATA.regexp)
+
+    def test_with_metadata(self):
+        self.assertIsNotNone(JPN.NationalID.METADATA)
+
+    def test_checksum(self):
+        self.assertTrue(JPN.NationalID.checksum("765895492872"))
+
+
+if __name__ == '__main__':
+    main()
