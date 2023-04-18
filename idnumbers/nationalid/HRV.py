e270a09bf5088adfb0106661fbba428e2844edd1
microdataxyz <microdataxyz@outlook.com>
2023-04-18 09:02:19 +0800

    Fix #244 - link Croatia TIN with OIB for both entity and individual (#259)

--- idnumbers/nationalid/HRV.py
+++ idnumbers/nationalid/HRV.py
@@ -1,6 +1,6 @@
+from types import SimpleNamespace
 from .hrv.personal_id import PersonalID
 from .util import alias_of
-
 
 NationalID = alias_of(PersonalID)
 """
@@ -9,4 +9,16 @@ alias of PersonalID
 OIB = alias_of(PersonalID)
 """
 alias of PersonalID
+"""
+PIN = alias_of(PersonalID)
+"""
+alias of PersonalID
+"""
+TIN = SimpleNamespace(**{
+    'individual': PersonalID,
+    'entity': PersonalID
+})
+"""
+According to the doc in https://www.porezna-uprava.hr/en/Pages/PIN.aspx, we know the TIN for individual and entity are
+the same.
 """
